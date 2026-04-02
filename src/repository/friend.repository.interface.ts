21718cf9b665accf7a5fc0bc3704eb47c4a1ae36
Hồ Việt Thắng <154068613+HoVietThang190704@users.noreply.github.com>
2026-04-02 10:27:41 +0700

    crud friends

---
+++ src/repository/friend.repository.interface.ts
@@ -0,0 +1,11 @@
+import { FriendResponseDto } from "../dto/friend.dto";
+import { Friend } from "../model/friend";
+
+export interface IFriendRepository {
+  addFriend(requesterId: string, recipientId: string): Promise<Friend>;
+  removeFriend(requesterId: string, recipientId: string): Promise<Friend>;
+  getFriends(userId: string): Promise<string[]>;
+  areFriends(userId: string, friendId: string): Promise<boolean>;
+  getIncomingRequests(userId: string): Promise<Friend[]>;
+  respondToRequest(requesterId: string, recipientId: string, status: "accepted" | "rejected" | "blocked"): Promise<Friend>;
+}
